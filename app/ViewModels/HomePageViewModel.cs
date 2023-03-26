0154a42fb5d2c3af936caeb268edc3c1ab88dfeb
liz171 <liz171@uni.coventry.ac.uk>
2023-03-26 13:39:00 +0800

    add swipeview and swipeitem, add delete command

--- app/ViewModels/HomePageViewModel.cs
+++ app/ViewModels/HomePageViewModel.cs
@@ -17,20 +17,46 @@ namespace app.ViewModels
 
         public HomePageViewModel()
         {
-            Title = "My Recipes";            
-        }
-
-        [RelayCommand]
-        void AddRecipe()
-        {
             MyRecipes.Add(new RecipeItem()
             {
                 Name = "Spring Minestrone Soup",
                 Image = "https://www.simplyrecipes.com/thmb/J0sEfuGpNA8_Q-_ju5k-KQDJagM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2011__05__spring-minestrone-vertical-b-1600-add4ea06b70d4a409b55f3dbb5f5f0ea.jpg",
                 Favorite = false,
-                BriefDescription = "Spring Soup"
+                BriefDescription = "this is a soup"
             });
         }
+
+        /// <summary>
+        /// delete this recipe
+        /// </summary>
+        /// <param name="recipe"></param>
+        [RelayCommand]
+        void DeleteRecipe(RecipeItem recipe)
+        {
+            if(recipe != null && MyRecipes.Contains(recipe))
+            {
+                MyRecipes.Remove(recipe);
+            }
+        }
+
+        /// <summary>
+        /// go to this recipe details page
+        /// </summary>
+        /// <param name="recipe"></param>
+        /// <returns></returns>
+        [RelayCommand]
+        async Task GoToDetails(RecipeItem recipe)
+        {
+            if (recipe == null)
+                return;
+
+            await Shell.Current.GoToAsync("DetailsPage", true, new Dictionary<string, object>
+            {
+                {"RecipeItem", recipe }
+            });
+        }
+
+
 
     }
 }
